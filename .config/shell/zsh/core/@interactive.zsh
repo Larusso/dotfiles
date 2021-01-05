a5f9d75260b7dc4700d3e2b0486424886ece7c6e
Manfred Endres <manfred.endres@tslarusso.de>
2021-01-05 18:09:39 +0100

    Add config and invocation for P10K theme

--- .config/shell/zsh/core/@interactive.zsh
+++ .config/shell/zsh/core/@interactive.zsh
@@ -1,11 +1,20 @@
 #
 # Core configuration module for zsh.
 #
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 xsh load core -s posix
 
 HISTSIZE="10000"
 SAVEHIST="10000"
 HISTFILE="${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.zsh_history"
+P10K_THEME_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.p10k.zsh"
 mkdir -p "$(dirname "$HISTFILE")"
 
 setopt HIST_FCNTL_LOCK
@@ -26,8 +35,7 @@ unsetopt EXTENDED_HISTORY
 #POWERLEVEL9K_STATUS_CROSS=true;
 #POWERLEVEL9K_PROMPT_ADD_NEWLINE=true;
 
-#. /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-. /usr/share/fzf/key-bindings.zsh
-. /usr/share/fzf/completion.zsh
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f $P10K_THEME_CONFIG ]] || source $P10K_THEME_CONFIG
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
