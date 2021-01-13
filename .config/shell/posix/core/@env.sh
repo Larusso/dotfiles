8b68cd54e5c50d579c60f1477e34ac45516273f1
Manfred Endres <manfred.endres@tslarusso.de>
2021-01-13 09:04:39 +0100

    Set GPG_TTY only on macOS

--- .config/shell/posix/core/@env.sh
+++ .config/shell/posix/core/@env.sh
@@ -1,9 +1,28 @@
 #
 # Core configuration module.
 #
+# Detect the platform (similar to $OSTYPE)
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin') 
+    OS='macOS'
+    ;;
+  *) ;;
+esac
+
 export LANG=en
 export PATH=/usr/local/bin:$PATH
-export GPG_TTY="$(tty)"
+
+if [ "$OS" = "macOS" ]; then
+    export GPG_TTY="$(tty)"
+fi
+
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 gpg-connect-agent updatestartuptty /bye >/dev/null
