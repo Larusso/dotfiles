d1808c8b16d8968c124171c887324fb286569ebc
Manfred Endres <manfred.endres@tslarusso.de>
2021-01-16 20:12:37 +0100

    Fix gnupg on macOS

--- .config/shell/posix/core/@env.sh
+++ .config/shell/posix/core/@env.sh
@@ -1,28 +1,3 @@
 #
 # Core configuration module.
 #
-# Detect the platform (similar to $OSTYPE)
-OS="`uname`"
-case $OS in
-  'Linux')
-    OS='Linux'
-    ;;
-  'WindowsNT')
-    OS='Windows'
-    ;;
-  'Darwin') 
-    OS='macOS'
-    ;;
-  *) ;;
-esac
-
-export LANG=en
-export PATH=/usr/local/bin:$PATH
-
-if [ "$OS" = "macOS" ]; then
-    export GPG_TTY="$(tty)"
-fi
-
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
-gpg-connect-agent updatestartuptty /bye >/dev/null
