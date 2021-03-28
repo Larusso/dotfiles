6235301c90d50f592fc7b3c2e4b3c442ca337489
Manfred Endres <manfred.endres@tslarusso.de>
2021-03-28 20:55:38 +0200

    Create generic polybar launch script

--- .config/yadm/alt/.config/polybar##os.Linux/launch.sh
+++ .config/yadm/alt/.config/polybar##os.Linux/launch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 killall -q polybar
+
+bar="${1:-$XDG_SESSION_DESKTOP}"
+
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload "$bar" &
 done
 echo "Polybar launched..."
