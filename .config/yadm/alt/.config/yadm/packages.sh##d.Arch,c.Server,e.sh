cb399c1db508b11ec26aa7ef559dca851f31c9ba
Manfred Endres <manfred.endres@tslarusso.de>
2021-12-14 19:50:14 +0100

    Split bootstrap only for packages list for now

---
+++ .config/yadm/alt/.config/yadm/packages.sh##d.Arch,c.Server,e.sh
@@ -0,0 +1,21 @@
+pacman_packages=(
+  #cli tools
+  exa
+  git-lfs
+  zsh
+  openssh
+  fzf
+  rustup
+  yadm
+  neovim
+  zsh-theme-powerlevel10k-git
+
+  #system
+  xdg-user-dirs
+  pacman-contrib
+  os-prober
+  libfido2
+  man-db
+  man-pages
+  powerline-console-fonts-git
+)
---
+++ .config/yadm/alt/.config/yadm/packages.sh##d.Arch,e.sh
@@ -0,0 +1,58 @@
+pacman_packages=(
+  # terminal
+  kitty
+  alacritty
+
+  #desktop
+  i3-gaps
+  i3lock
+  i3status
+  lightdm
+  lightdm-slick-greeter
+  polybar
+  picom-jonaburg-git
+  lxappearance
+  arc-gtk-theme
+
+  #fonts
+  apple-fonts
+  nerd-fonts-droid-sans-mono
+  nerd-fonts-iosevka
+  ttf-meslo-nerd-font-powerlevel10k
+
+  #cli tools
+  exa
+  git-lfs
+  zsh
+  openssh
+  fzf
+  rustup
+  yadm
+  neovim
+  pmount-safe-removal
+  zsh-theme-powerlevel10k-git
+
+  #applications
+  sxiv
+  feh
+  firefox
+  dmenu
+  rofi
+  mailspring
+  pcmanfm-gtk3
+  zeal
+
+  #system
+  nvidia
+  xorg-xinit
+  xorg-xrandr
+  xdg-user-dirs
+  nvidia-settings
+  pacman-contrib
+  pavucontrol
+  os-prober
+  gnome-keyring
+  powerline-console-fonts
+  man-db
+  man-pages
+)
