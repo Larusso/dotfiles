e67203328ef398856ac855a59ad512bd67b60385
Manfred Endres <manfred.endres@tslarusso.de>
2024-11-09 14:41:04 +0100

    Add git delta and fix exa

--- .config/shell/posix/core/@env.sh
+++ .config/shell/posix/core/@env.sh
@@ -5,5 +5,6 @@
 export LANG=en_US.UTF-8
 export PATH=/opt/homebrew/bin:/usr/local/bin:$HOME/.local/bin:$PATH
 export PATH="$PATH:/Users/larusso/.dotnet/tools"
+export PATH="$PATH:/Users/larusso/.dotnet"
 export LC_ALL=en_US.UTF-8
 
--- .config/shell/posix/core/@interactive.sh
+++ .config/shell/posix/core/@interactive.sh
@@ -4,7 +4,7 @@
 
 alias reload='exec "$XSHELL"' # reload the current shell configuration
 
-alias ls="exa"
+alias ls="eza"
 alias ll="ls -l"
 alias la="ls -la"
 alias gradle="gw"
@@ -73,4 +73,4 @@ export FZF_DEFAULT_OPTS='--no-reverse'
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 export FZF_CTRL_T_OPTS="--select-1 --exit-0"
 export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
-export FZF_ALT_C_OPTS="--preview 'exa --tree {} | head -200'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree {} | head -200'"
