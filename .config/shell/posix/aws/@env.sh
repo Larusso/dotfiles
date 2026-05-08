a96b00c6e6fcbacd49645b499e0486066543d09e
Manfred Endres <manfred.endres@tslarusso.de>
2026-05-08 12:03:16 +0200

    Add aws-ss-creds helper tool and environment variable

--- .config/shell/posix/aws/@env.sh
+++ .config/shell/posix/aws/@env.sh
@@ -4,6 +4,7 @@
 
 export AWS_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/aws/config"
 export AWS_SHARED_CREDENTIALS_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/aws/credentials"
+export AWS_SSO_BROWSER="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 export SAML2AWS_CONFIGFILE="${XDG_CONFIG_HOME:-$HOME/.config}/saml2aws/config"
 export SAML2AWS_CREDENTIALS_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/saml2aws/credentials"
